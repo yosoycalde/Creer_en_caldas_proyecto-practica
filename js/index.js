1cbdad118e79d5d6dac2d949330801f18e33e90e
yosoycalde <juancalderon2005.18@gmail.com>
2025-06-10 14:01:55 -0500

    10/06/2025 10

--- js/index.js
+++ js/index.js
@@ -9,20 +9,30 @@ function plusSliders(n) {
 }
 // currentSlide(n) : nos sirve para mostrar una diapositiva especifica
 function currentSlide(n) {
+    // muestra la diapositiva 
     showSlides(slideIndex = n);
 }
+
 function showSlides(n) {
+    // variables locales
     let i;
+    // obtiene todos los elementos con clase "sliders" (las diapositivas)
     let slides = document.querySelectorAll(".sliders");
+    // obtenemos todos los puntos de navegacion con la clse "carr"
     let dots = document.querySelectorAll(".carr");
-    if (n > slides.length) slideIndex = 1;
-    if (n < 1) slideIndex = slides.length;
+    // manejo de ciclo del carrucel con contador 
+    if (n > slides.length) slideIndex = 1; //si llega al final vueve al inicio
+    if (n < 1) slideIndex = slides.length; //si vamos antes de el inicio nos lleva a el final
+    // oculta todas las diapositivas
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    // remueve la clase "active" de todos los puntos
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+    // Muestra la diapositiva actual 
     slides[slideIndex - 1].style.display = "block";
+    // Marca el punto correspondiente como activo 
     dots[slideIndex - 1].className += " active";
 }
